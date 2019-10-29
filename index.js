d70ce425c88dec64519e670b8dc2693f8b2c82f6
Vyraal1 <kevin.afable@gmail.com>
2019-10-29 10:54:51 -0400

    scuffed get request by ID, not sure if it works

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ server.get("/", (req, res) => {
 });
 
 server.get("/api/users", (req, res) => {
+  console.log(req.body);
   users
     .find()
     .then(users => {
       res.status(200).json(users);
     })
-    .catch(err => {
+    .catch(() => {
       res
         .status(500)
         .json({ err: "The users information could not be retrieved." });
@@ -30,17 +31,42 @@ server.post("/api/users", (req, res) => {
       .then(user => {
         res.status(201).json(user);
       })
-      .catch(err => {
+      .catch(() => {
         res.status(500).json({
-          error: "There was an error while saving the user to the database"
+          errorMsg: "There was an error while saving the user to the database"
         });
       });
   } else {
     //if not do what the readme says
     res
       .status(400)
-      .json({ errorMessage: "Please provide name and bio for the user." });
+      .json({ errorMsg: "Please provide name and bio for the user." });
   }
+});
+
+//getting users by their ID
+server.get(`api/users/:id`, (req, res) => {
+  // assuming I didn't have videos or TK to lean on
+  // where tf are the id's in the request?
+  console.log(req.params);
+  users
+    .findById(req.params.id)
+    .then(user => {
+      //return the user that was found
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        //if not found, user should be an empty array, resolving to false
+        res
+          .status(404)
+          .json({ errorMsg: "The user information could not be retrieved." });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMsg: "The user information could not be retrieved." });
+    });
 });
 
 server.listen(8000, () => {
